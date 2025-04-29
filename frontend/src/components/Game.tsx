aee4c404253a103b97656067538bf092e94d9711
ElitoGame <elitogame123@gmail.com>
2025-04-29 16:08:27 +0200

    Emergency Meeting logic start & Game files

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,6 +1,29 @@
+import { onCleanup, onMount } from "solid-js";
 import { setGameState, setPlayerData } from "../App";
 
 const Game = () => {
+	const ndef = new NDEFReader();
+	onMount(async () => {
+		try {
+			await ndef.scan();
+
+			ndef.addEventListener("readingerror", () => {
+				console.log(
+					"readLog",
+					"Argh! Cannot read data from the NFC tag. Try another one?"
+				);
+			});
+
+			ndef.addEventListener("reading", handleReading);
+		} catch (error) {
+			// log("readLog", "Argh! " + error);
+		}
+	});
+
+	onCleanup(() => {
+		ndef.removeEventListener("reading", handleReading);
+	});
+
 	return (
 		<div>
 			<h1>Game</h1>
@@ -21,3 +44,43 @@ const Game = () => {
 };
 
 export default Game;
+
+export async function handleReading(event: Event) {
+	const { message, serialNumber } = event as NDEFReadingEvent;
+	const decoder = new TextDecoder();
+	for (const record of message.records) {
+		switch (record.recordType) {
+			case "text":
+				const textDecoder = new TextDecoder(record.encoding);
+				// log(
+				//     "readLog",
+				//     `Text: ${textDecoder.decode(record.data)} (${
+				//         record.lang
+				//     })`
+				// );
+				alert(textDecoder.decode(record.data));
+				break;
+			case "url":
+				// log("readLog", `URL: ${decoder.decode(record.data)}`);
+				if (decoder.decode(record.data) === import.meta.env.VITE_APP_URL) {
+					alert("Emergency Meeting called!");
+					await fetch("https://among-us-irl.mcdle.net/emergency", {
+						method: "POST",
+					});
+				}
+				break;
+			case "mime":
+				if (record.mediaType === "application/json") {
+					// log(
+					//     "readLog",
+					//     `JSON: ${JSON.parse(decoder.decode(record.data))}`
+					// );
+				} else {
+					// log("readLog", `Media not handled`);
+				}
+				break;
+			default:
+			// log("readLog", `Record not handled`);
+		}
+	}
+}
