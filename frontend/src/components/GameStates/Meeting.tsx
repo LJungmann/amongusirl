ca4299190f6db4aeddef5180f3f951dcea47cc2b
Lokinaira <75321001+Lokinaira@users.noreply.github.com>
2025-05-07 16:14:55 +0200

    Started working on the Meeting UI + added alive sprites for all colors

--- frontend/src/components/GameStates/Meeting.tsx
+++ frontend/src/components/GameStates/Meeting.tsx
@@ -1,4 +1,4 @@
-import { For, Show } from "solid-js";
+import { For, onMount, Show } from "solid-js";
 import { gameStateData, playerData } from "../../App";
 import { isPlayerAlive, isPlayerRegisteredForVoting } from "../../utils";
 
@@ -19,40 +19,114 @@ const Meeting = () => {
 			<Show
 				when={isPlayerRegisteredForVoting()}
 				fallback={
-					<p class="text-red-800">You are not yet registered for voting!</p>
+					<div class="h-full items-center justify-center flex">
+						<p class="text-red-800">You are not yet registered for voting!</p>
+					</div>
 				}
 			>
-				<p class="text-green-800">You are registered for voting!</p>
-			</Show>
-			<p class="text-red-800">Emergency Meeting!</p>
-			<p>Meeting over in: {getMeetingEndTime()}</p>
-			<ul>
-				<For
-					each={gameStateData().alivePlayers}
-					fallback={<p>No players to vote for</p>}
-				>
-					{(x) => (
-						<li>
+				<div class="flex flex-col gap-2 mx-4">
+					<h2
+						class="text-red-800 text-5xl text-center pt-4"
+						style={{ "font-family": "VCR_OSD_MONO" }}
+					>
+						Emergency Meeting!
+					</h2>
+					<div class="relative">
+						<span class="absolute top-1/2 left-1/2 text-xl font-bold -translate-1/2">
+							Meeting over in: {getMeetingEndTime()}
+						</span>
+						<progress
+							//meeting start time causes issues because it is already too long ago when players have registered for voting
+							max={
+								(gameStateData().meetingEndTime -
+									gameStateData().meetingStartTime) /
+								1000
+							}
+							value={
+								(gameStateData().meetingEndTime - new Date().getTime()) / 1000
+							}
+							// meeting endet um 10 Uhr
+							// es ist gerade 9:55 Uhr
+							// meeting dauert noch 5 min
+							//um 9:57 dauert das meeting noch 3 min
+							class="w-full h-12 progressbar"
+						/>{" "}
+					</div>
+					<Show when={!isPlayerAlive()}>
+						<p class="text-red-800 w-full text-center py-2">
+							You are dead and cannot vote!
+						</p>
+					</Show>
+					<ul class="flex flex-row flex-wrap gap-2 py-4 items-center justify-center rounded bg-gray-200">
+						<For
+							// each={gameStateData().alivePlayers}
+							each={[
+								{ playerId: 0 },
+								{ playerId: 1 },
+								{ playerId: 2 },
+								{ playerId: 3 },
+								{ playerId: 4 },
+								{ playerId: 5 },
+								{ playerId: 6 },
+								{ playerId: 7 },
+								{ playerId: 8 },
+								{ playerId: 9 },
+							]}
+							fallback={<p>No players to vote for</p>}
+						>
+							{(x) => (
+								<li class="w-[45%] bg-red-500 hover:bg-red-700 flex flex-row py-4 gap-2 rounded justify-center">
+									<img src="/Among_Us_Crewmate.webp" class="w-8 h-fit" />
+									<button
+										class="text-white font-bold w-fit"
+										onClick={async () => {
+											if (!isPlayerAlive()) {
+												alert("You are dead! You cannot vote!");
+												return;
+											}
+											alert(
+												"Voted for " +
+													x.playerId +
+													" successfully! as " +
+													typeof x.playerId,
+											);
+											const response = await fetch(
+												"https://among-us-irl.mcdle.net/voteFor",
+												{
+													method: "POST",
+													//"" + x.playerId,
+													body: JSON.stringify({
+														playerId: x.playerId,
+													}),
+													headers: {
+														"Content-Type": "application/json",
+													},
+												},
+											);
+											alert(
+												(await response.text()) === "false"
+													? "Failed"
+													: "Success",
+											);
+										}}
+									>
+										Vote for {x.playerId + 1} (You)
+										{x.playerId === playerData().playerId ? "(You)" : ""}
+									</button>
+								</li>
+							)}
+						</For>
+						<li class="w-[45%] bg-red-500 hover:bg-red-700 flex flex-row py-4 gap-2 rounded justify-center">
 							<button
-								class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+								class="text-white font-bold w-fit"
 								onClick={async () => {
-									if (!isPlayerAlive()) {
-										alert("You are dead! You cannot vote!");
-										return;
-									}
-									alert(
-										"Voted for " +
-											x.playerId +
-											" successfully! as " +
-											typeof x.playerId,
-									);
 									const response = await fetch(
 										"https://among-us-irl.mcdle.net/voteFor",
 										{
 											method: "POST",
 											//"" + x.playerId,
 											body: JSON.stringify({
-												playerId: x.playerId,
+												playerId: -1,
 											}),
 											headers: {
 												"Content-Type": "application/json",
@@ -64,13 +138,12 @@ const Meeting = () => {
 									);
 								}}
 							>
-								Vote for {x.playerId}{" "}
-								{x.playerId === playerData().playerId ? "(You)" : ""}
+								Skip
 							</button>
 						</li>
-					)}
-				</For>
-			</ul>
+					</ul>
+				</div>
+			</Show>
 		</div>
 	);
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
 	server: {
 		port: 3001,
 		allowedHosts: [
-			"needle-plastics-rocket-quantum.trycloudflare.com",
+			"compromise-enrolled-father-leone.trycloudflare.com",
 			"among-us.mcdle.net",
 		],
 	},
